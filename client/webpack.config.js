dd29c29d69433806190094808c5bc165b3186110
Reese Hartnett <juliapoole44@gmail.com>
2022-05-10 11:17:21 -0400

    configure plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Edit some text!',
+        background_color: '#003b6f',
+        theme_color: '#003b6f',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
@@ -31,6 +59,17 @@ module.exports = () => {
         //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
         //   type: 'assets/resource',
         // },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        },
       ],
     },
   };
