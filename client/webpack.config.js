557349b52c5c816bc3d3f02c78bfedfeb699dcc2
Reese Hartnett <juliapoole44@gmail.com>
2022-05-10 10:55:53 -0400

    implement asset caching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,14 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // {
+        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        //   type: 'assets/resource',
+        // },
       ],
     },
   };
